e18883a4bf487ce3da3f45b2eba25bb5018eacf7
OwlWilderness <98717833+OwlWilderness@users.noreply.github.com>
2022-07-07 03:38:36 +0000

    Update Home.jsx

--- packages/react-app/src/views/Home.jsx
+++ packages/react-app/src/views/Home.jsx
@@ -1,8 +1,9 @@
 import { useContractReader } from "eth-hooks";
 import { ethers } from "ethers";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import {Address} from "../components";
 
 /**
  * web3 props can be passed from '../App.jsx' into your local view component for use
@@ -16,33 +17,50 @@ function Home({ yourLocalBalance, readContracts }) {
   const uri = useContractReader(readContracts, "GNDG", "uri", "1");
   const wvrp = useContractReader(readContracts, "GNDG", "wvrp");
   const lastWvrp = useContractReader(readContracts, "GNDG", "lastWvrp");
+  const pricefeed = useContractReader(readContracts, "GNDG", "priceFeed");
 
-  const getTokenURI = async () => {
+  //console.log("tokenURI",uri);
+  const [metadata, setMetadata] = useState();
 
-    //const tokenURI = useContractReader(readContracts, "GNDG", "uri", "1");
-    console.log("tokenURI",uri);
+  const getTokenURI = async (uri) => {
     try{
       const metadata = await axios.get(uri);
+
       if(metadata){
+        //return metadata.data
+        setMetadata(metadata.data)
+        console.log("metadata.data", metadata.data)
         return { ...metadata.data, uri /*, approved: approved === writeContracts.GigaNFT.address */ };
       }
     }catch(e){console.log(e)}
 
-    //console.log("metadata",metadata.data)
-    //const approved = await readContracts.GigaNFT.getApproved(id);
-
   };
+
+  useEffect(() => {
+    if (uri) getTokenURI(uri);
+  }, [uri, readContracts]);
 
   return (
     <div>
       <div style={{ margin: 32 }}>
         <span style={{ marginRight: 8 }}>📝</span>
-        Aclhemy Road 2 Web 3 Challenge in Scaffold-Eth{" "}
+        <a href="https://mumbai.polygonscan.com/address/0xa40b1c58fb12D2b7f664D482cF4A16186f35016f" target="_blank">ERC-1155 on Mumbai</a> Implementation of <a href="https://docs.alchemy.com/alchemy/road-to-web3/weekly-learning-challenges/5.-connect-apis-to-your-smart-contracts-using-chainlink" target="_blank">
+          Aclhemy Road 2 Web 3 Week 5</a>{" "} using <a href="https://github.com/scaffold-eth/scaffold-eth" target="blank">Scaffold-Eth</a> <br></br> (chainlink datafeeds and vrf)
+          <p></p> Get Started With Scaffold-Eth @ <a href="https://speedrunethereum.com/" target="_blank">Speed Run Ethereum</a>
       </div>
-
+      <div style={{ margin: 32 }}>
+        <span style={{ marginRight: 8 }}>🛠</span>
+        Tinker with the GNDG smart contract using the <Link to="/gndg">"Debug GNDG"</Link> tab.
+      </div>
+      <hr></hr>
+      <div style={{ margin: 32 }}>
+        gnar and glitch seem to appear during a downward instant 
+        <br></br> nay goon and glitching seem to appear during an upward instant
+        <p></p> pricefeed: <Address address={pricefeed} fontSize={12} size={'long'}/>
+      </div>
       <div style={{ margin: 32 }}>
         <span style={{ marginRight: 8 }}>🤖</span>
-        last wvrp: {lastWvrp} <p></p> current wvrp: 
+        last wvrp: {lastWvrp} <br></br> current wvrp: 
         <span
           className="highlight"
           style={{ marginLeft: 4, /* backgroundColor: "#f9f9f9", */ padding: 4, borderRadius: 4, fontWeight: "bolder" }}
@@ -50,22 +68,13 @@ function Home({ yourLocalBalance, readContracts }) {
           {wvrp}
         </span>{" "}
 
-        <p></p>json uri:
-        
-        <span
-          className="highlight"
-          style={{ marginLeft: 4, /* backgroundColor: "#f9f9f9", */ padding: 4, borderRadius: 4, fontWeight: "bolder" }}
-        >
-          {uri}
-        </span>
-
+        <p></p>{metadata && <img src={metadata.image} width="400" height="400"></img>}
       </div>
 
-      <div style={{ margin: 32 }}>
-        <span style={{ marginRight: 8 }}>🛠</span>
-        Tinker with the GNDG smart contract using the <Link to="/gndg">"Debug GNDG"</Link> tab.
-      </div>
+
+      
     </div>
+    
   );
 }
 
